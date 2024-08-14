5bf8f51a2a28f70773124d6693b443237cb96561
bcmray_crazy <975875040@qq.com>
2024-08-14 11:04:55 +0800

    优化用户体验

---
+++ components/serverUrlSnippet.tsx
@@ -0,0 +1,35 @@
+import { Snippet } from "@nextui-org/snippet";
+import { Component } from "react";
+
+type _State = {
+  hover: boolean;
+};
+
+export class ServerUrlSnippet extends Component<undefined, _State> {
+  constructor() {
+    super(undefined);
+    this.state = { hover: false };
+  }
+
+  mouseEnter = () => {
+    this.setState({ hover: true });
+  };
+  mouseLeave = () => {
+    this.setState({ hover: false });
+  };
+
+  render() {
+    return (
+      <Snippet
+        className="text-2xl px-4 py-4 border-secondary border-2"
+        color={this.state.hover ? "default" : "secondary"}
+        symbol=""
+        variant="shadow"
+        onMouseEnter={this.mouseEnter}
+        onMouseLeave={this.mouseLeave}
+      >
+        <span style={{ fontFamily: "Mojangles" }}>mc.hjfunny.site</span>
+      </Snippet>
+    );
+  }
+}
--- components/shinings.tsx
+++ components/shinings.tsx
@@ -1,18 +1,18 @@
 "use client";
 import { Card, CardHeader, CardBody } from "@nextui-org/card";
-import { IconContext } from "react-icons";
+// import { IconContext } from "react-icons";
 import { AiOutlineFullscreen, AiOutlineAppstoreAdd } from "react-icons/ai";
 import { BiMessageRoundedCheck } from "react-icons/bi";
 
-function IconWrapper({ children }: any) {
-  return (
-    <>
-      <IconContext.Provider value={{ size: "3rem" }}>
-        {children}
-      </IconContext.Provider>
-    </>
-  );
-}
+// function IconWrapper({ children }: any) {
+//   return (
+//     <>
+//       <IconContext.Provider value={{ size: "64px" }}>
+//         {children}
+//       </IconContext.Provider>
+//     </>
+//   );
+// }
 
 function ShiningsCard(props: {
   title: string;
@@ -22,8 +22,8 @@ function ShiningsCard(props: {
   return (
     <Card isHoverable className="flex-1 mb-3 mr-3">
       <CardHeader>
-        <IconWrapper>{props.children}</IconWrapper>
-        <h4 className="font-bold text-lg m-0">{props.title}</h4>
+        {/* <IconWrapper>{props.children}</IconWrapper> */}
+        <h4 className="font-bold text-3xl m-0">{props.title}</h4>
       </CardHeader>
       <CardBody>
         <p className="m-0 w-[90%]">{props.content}</p>
@@ -36,19 +36,16 @@ export function Shinings() {
   return (
     <div className="flex flex-col lg:flex-row mt-7">
       <ShiningsCard
-        content="服务器玩法多样，生存、生电、建筑、小游戏……"
+        content="服务器玩法多样, 生存、建筑、小游戏……应有尽有"
         title="多元"
       >
         <AiOutlineAppstoreAdd />
       </ShiningsCard>
-      <ShiningsCard
-        content="我们的服务器群组是开放的, 任何服务器都可以与我们联合"
-        title="开放"
-      >
+      <ShiningsCard content="服务器开放, 欢迎各位腐竹联合开服" title="开放">
         <AiOutlineFullscreen />
       </ShiningsCard>
       <ShiningsCard
-        content="我们支持跨服聊天, 你可以与另一个服务器的玩家畅快交流, 消除服务器之间的隔阂"
+        content="服务器支持跨服聊天, 随处与另一个服务器的玩家畅快交流, 服务器之间不再有隔阂"
         title="共生"
       >
         <BiMessageRoundedCheck />
