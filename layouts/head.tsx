16543ab796eefc5e6445fdb9617de273334ebcbd
CodeZhangBorui <zhangborui.2008@foxmail.com>
2024-08-18 18:53:34 +0800

    feat: Add meta keywords

--- layouts/head.tsx
+++ layouts/head.tsx
@@ -11,6 +11,10 @@ export const Head = ({ title }: { title: string }) => {
       <meta content={siteConfig.description} property="og:description" />
       <meta content={siteConfig.description} name="description" />
       <meta
+        content="Minecraft, Minecraft Server, hjfunnyMC, hjmc"
+        name="keywords"
+      />
+      <meta
         key="viewport"
         content="viewport-fit=cover, width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
         name="viewport"
